61ffe499c75315f8b3f0b573757b257f9f965bde
kennisnutz <kenneth.hotson83@gmail.com>
2022-08-15 12:31:49 +0000

    reconfigurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+
+const ALCHEMY_URL = process.env.REACT_APP_ALCHEMY_PROJECT_ID  as string;
+const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY as string;
+// const BUYER_PRIVATE_KEY = process.env.BUYER_PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.11",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_URL,
+      accounts: [OWNER_PRIVATE_KEY],
+    },
+    // harmonyTestnet: {
+    //   url: "",
+    //   accounts: [OWNER_PRIVATE_KEY],
+    // },
+    // rinkeby: {
+    //   url: "",
+    //   accounts: [OWNER_PRIVATE_KEY],
+    // },
+    // bscTestnet: {
+    //   url: ""
+    //   accounts: [OWNER_PRIVATE_KEY],
+    // },
+  },
 };
 
 export default config;
